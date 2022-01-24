5b4915d8465310917d8b30e87b42a53fe5c1b539
Alvaro <alvaro.fernandez@iesnervion.es>
2022-01-24 19:42:33 +0100

    Model

--- Trabajo-DEINT-Pasapalabra/Trabajo-DEINT-PasapalabraUI/Models/clsModelPregunta.cs
+++ Trabajo-DEINT-Pasapalabra/Trabajo-DEINT-PasapalabraUI/Models/clsModelPregunta.cs
@@ -14,12 +14,13 @@ namespace Trabajo_DEINT_PasapalabraUI.Models
 
         #endregion
         #region constructores
-<<<<<<< HEAD
         public clsModelPregunta(int estado, int id, string pregunta, string respuesta, char letra) :
-            base(id, pregunta, respuesta,letra)
-=======
-        public clsModelPregunta(int estado,int id,string pregunta, string respuesta):base(id,pregunta,respuesta)
->>>>>>> 6172b6dd6902474b2791390465b0ccde7467fb66
+            base(id, pregunta, respuesta, letra)
+        {
+            Estado = estado;
+        }
+
+        public clsModelPregunta(int estado,int id,string enunciado, string respuesta):base(id,enunciado,respuesta)
         {
             Estado = estado;
         }
