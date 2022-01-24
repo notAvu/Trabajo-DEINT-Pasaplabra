904670416161c0425a45c5f52a5aacbdf50eadf5
jmunoz <rypperx@gmail.com>
2022-01-24 14:33:07 +0100

    Modelo clsModelPregunta realizado.

---
+++ Trabajo-DEINT-Pasapalabra/Trabajo-DEINT-PasapalabraUI/Models/clsModelPregunta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trabajo_DEINT_PasapalabraEntities;
+
+namespace Trabajo_DEINT_PasapalabraUI.Models
+{
+    class clsModelPregunta : clsPartida
+    {
+        #region propiedadesAutoimplementadas
+        public int Estado { get; set; }
+        #endregion
+        #region constructores
+        public clsModelPregunta(int estado, int id, string nick, int totalAcertadas, int totalFalladas, DateTime tiempo
+            ) :base(id,nick,totalAcertadas,totalFalladas,tiempo )
+        {
+            Estado = estado;
+        }
+        #endregion
+    }
+}
