6a0c8c2e75703dbe2f320dd91c64c5692da0a21f
Álvaro Fernández <avunotfound@gmail.com>
2022-01-26 13:39:49 +0100

    German no me pegues, solo he tocado el vm

---
+++ Trabajo-DEINT-Pasapalabra/Trabajo-DEINT-PasapalabraUI/ViewModels/clsGameVM.cs
@@ -0,0 +1,41 @@
+﻿using _18_CRUD_Personas_UWP_UI.ViewModels.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trabajo_DEINT_PasapalabraUI.Models;
+
+namespace Trabajo_DEINT_PasapalabraUI.ViewModels
+{
+    class clsGameVM : clsVMBase
+    {
+        #region propiedades privadas
+        List<clsModelPregunta> listadoPreguntas;
+        clsModelPregunta preguntaSeleccionada;//mejor indice seleccionado?
+        int aciertos;
+        int fallos;
+        string respuestaJugador;
+        DateTime tiempoRestante;//TODO TIEMPO EN SEGUNDOS O CON MINUTERO?
+        #endregion
+        #region constructor
+        public clsGameVM()
+        {
+            listadoPreguntas = new List<clsModelPregunta>();
+            preguntaSeleccionada = listadoPreguntas[0];
+            aciertos = 0;
+            fallos = 0;
+            tiempoRestante = new DateTime();
+            respuestaJugador = "";
+        }
+        #endregion
+        #region propiedades publicas
+        public string RespuestaJugador { get => respuestaJugador; set => respuestaJugador = value; }
+        public DateTime TiempoRestante { get => tiempoRestante; set => tiempoRestante = value; }
+        public int Aciertos { get => aciertos; set => aciertos = value; }
+        public int Fallos { get => fallos; set => fallos = value; }
+        internal clsModelPregunta PreguntaSeleccionada { get => preguntaSeleccionada; set => preguntaSeleccionada = value; }
+        internal List<clsModelPregunta> ListadoPreguntas { get => listadoPreguntas; set => listadoPreguntas = value; }
+        #endregion
+    }
+}
