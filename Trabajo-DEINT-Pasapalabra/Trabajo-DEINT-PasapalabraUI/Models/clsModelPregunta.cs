696205d33c49f84cec6da78ef22a91f43718da04
Álvaro Fernández <75076925+notAvu@users.noreply.github.com>
2022-02-18 12:24:29 +0100

    Cambio de color segun el estado de la pregunta

--- Trabajo-DEINT-Pasapalabra/Trabajo-DEINT-PasapalabraUI/Models/clsModelPregunta.cs
+++ Trabajo-DEINT-Pasapalabra/Trabajo-DEINT-PasapalabraUI/Models/clsModelPregunta.cs
@@ -1,16 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Trabajo_DEINT_PasapalabraEntities;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
 
 namespace Trabajo_DEINT_PasapalabraUI.Models
 {
     public class clsModelPregunta : clsPregunta
     {
+        private int estado;
         #region propiedadesAutoimplementadas
-        public int Estado { get; set; }
+        public int Estado { get => estado; set { estado = value; EvaluarColor(); } }
+
+        private void EvaluarColor()
+        {
+            if (estado == 1)
+            {
+                Color.Color = Colors.Green;
+            }
+            else if (estado == -1)
+            {
+                Color.Color = Colors.Red;
+            }
+        }
+
+        public SolidColorBrush Color { get; set; }
 
         #endregion
         #region constructores
@@ -18,11 +35,18 @@ namespace Trabajo_DEINT_PasapalabraUI.Models
             base(id, pregunta, respuesta, letra)
         {
             Estado = estado;
+            Color = new SolidColorBrush(Colors.Blue);
         }
 
-        public clsModelPregunta(int estado,int id,string enunciado, string respuesta):base(id,enunciado,respuesta)
+        public clsModelPregunta(int estado, int id, string enunciado, string respuesta) : base(id, enunciado, respuesta)
         {
             Estado = estado;
+            Color = new SolidColorBrush(Colors.Blue);
+        }
+        #endregion
+        #region animaciones
+        public void SelectedAnimation() {
+            
         }
         #endregion
     }
