9a8e652e2256a33238ea239738883aea4d00b3ec
Mar <mar.accri@gmail.com>
2022-03-04 13:50:43 +0100

    intento de tabla

--- Trabajo-DEINT-Pasapalabra/Trabajo-DEINT-PasapalabraUI/Views/Ranking.xaml.cs
+++ Trabajo-DEINT-Pasapalabra/Trabajo-DEINT-PasapalabraUI/Views/Ranking.xaml.cs
@@ -26,5 +26,10 @@ namespace Trabajo_DEINT_PasapalabraUI.Views
         {
             this.InitializeComponent();
         }
+        
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GamePage));
+        }
     }
 }
